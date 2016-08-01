14540256e4445460f27ea017a4409dc46f09ed29
Rodrigo Alfonso <rodri042@gmail.com>
2016-08-01 04:00:49 -0300

    Deleted mobile services mocks

--- desktop/src/com/rartworks/engine/desktop/InAppBillingServicesMock.kt
+++
@@ -1,8 +0,0 @@
-package com.rartworks.engine.desktop
-
-import com.rartworks.engine.apis.InAppBillingServices
-
-class InAppBillingServicesMock : InAppBillingServices {
-	override fun startPurchase(sku: String) {
-	}
-}
